d147116a01a9225334024d51747351969d97ccef
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-02-07 10:50:03 +0100

    Add files via upload

---
+++ oop1.py
@@ -0,0 +1,16 @@
+# Program nalazi nagib i udaljenost između dvije točke
+class Line(object):
+    
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return ((x2-x1)**2 + (y2-y1)**2)**0.5
+    
+    def slope(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return (y2-y1)/(x2-x1)
