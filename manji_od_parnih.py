dfbc603ade8e6232e04cd7e9d02f6174d89f4830
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-04-22 18:54:20 +0200

    Delete manji_od_parnih.py

--- manji_od_parnih.py
+++
@@ -1,6 +0,0 @@
-# Program vraća manji broj od dva parna, a ako je jedan ili oba neparan vraća veći
-def lesser_of_two_evens(a,b):
-    if a%2 == 0 and b%2 == 0:
-        return min(a,b)
-    else:
-        return max(a,b)
