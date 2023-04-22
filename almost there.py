4e492f2e63b53f97bff2e3e4f9b4ee2147af1e3b
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-04-22 18:55:33 +0200

    Delete almost there.py

--- almost there.py
+++
@@ -1,6 +0,0 @@
-# Program gleda jel broj udaljen za 10 od 100 ili 200
-def almost_there(n):
-    if (n > 89 and n < 111) or (n > 189 and n < 211):
-        return True
-    else:
-        return False
