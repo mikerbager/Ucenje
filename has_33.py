a251b2e902eeb67353035149c4379af09220d795
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-04-22 18:54:34 +0200

    Delete has_33.py

--- has_33.py
+++
@@ -1,8 +0,0 @@
-# Program gleda da li u listi imaju dva broja 3 za redom
-def has_33(nums):
-    
-    for i in range(0,len(nums)-1):
-        if nums[i] == 3 and nums[i+1] == 3:
-            return True
-    
-    return False
