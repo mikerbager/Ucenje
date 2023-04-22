8b62d11f9c1e88887e993b6ed2687aa073aee34d
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-04-22 18:54:12 +0200

    Delete master yoda.py

--- master yoda.py
+++
@@ -1,9 +0,0 @@
-# Program vraća rečenicu s riječima u obrnutom redoslijedu
-def master_yoda(text):
-    wordlist=text.split()
-    newlist=[]
-    i=(-1)
-    while i >= len(wordlist)*(-1):
-        newlist.append(wordlist[i])
-        i=i-1
-    return ' '.join(newlist)
