baeb76290a7e1742385b911c56991402da8c657a
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-04-22 18:52:10 +0200

    Delete uniquelist.py

--- uniquelist.py
+++
@@ -1,9 +0,0 @@
-# Program briše članove liste koji se ponavljaju
-def unique_list(lst):
-    seen_numbers=[]
-    for number in lst:
-        if number not in seen_numbers:
-            seen_numbers.append(number)
-        else:
-            pass
-    return seen_numbers
