83fea3b00b2ca78350b4eb13488a03b0e3e73f18
mikerbager <124389217+mikerbager@users.noreply.github.com>
2023-04-22 18:53:20 +0200

    Delete palindrom.py

--- palindrom.py
+++
@@ -1,7 +0,0 @@
-# Program provjerava palindrom
-def palindrome(s):
-    s=s.lower().replace(' ','')
-    if s[::] == s[::-1]:
-        return 'To je palindrom'
-    else:
-        return 'Nije palindrom'
